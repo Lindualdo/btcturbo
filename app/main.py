e7ed6f7ca7b10bff964c5b2ee2bc081b85bb53e3
Lindualdo <lindualdo@hotmail.com>
2025-05-27 13:08:53 +0100

    feet: inclusão do roout analise no app

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 # app/main.py
 
 from fastapi import FastAPI
-from app.routers import coleta, indicadores,score
+from app.routers import coleta, indicadores,score,analise
 
 app = FastAPI(
     title="BTC Turbo API",
@@ -9,6 +9,14 @@ app = FastAPI(
     version="1.0.1"
 )
 
-app.include_router(coleta.router, prefix="/api/v1", tags=["API v1"])
-app.include_router(indicadores.router, prefix="/api/v1", tags=["API v1"])
+# 1 - coleta indicadores (origens diversas) e grava no postgres
+app.include_router(coleta.router, prefix="/api/v1", tags=["API v1"]) 
+
+#2 - obter os indicadore (dados brutos) no postgres
+app.include_router(indicadores.router, prefix="/api/v1", tags=["API v1"]) 
+
+#3 - calcular score dos indicadores de cada bloco (usa sempre os dados brutos do postgres)
 app.include_router(score.router, prefix="/api/v1", tags=["API v1"])
+
+#4 - score consolidado (usa os scores consolidados de cada bloco) 
+app.include_router(analise.router, prefix="/api/v1", tags=["API v1"]) 
