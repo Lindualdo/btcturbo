f777fcee05a8630ab7a54971bd16e2a859839167
Lindualdo <lindualdo@hotmail.com>
2025-05-31 17:03:49 +0100

    fix - helpers - init.py

--- app/services/utils/helpers/__init__.py
+++ app/services/utils/helpers/__init__.py
@@ -5,23 +5,23 @@ PostgreSQL Helpers - BTC Turbo v1.0.14
 Cache diário + helpers por bloco
 """
 
-# Imports da base
-from .base import get_db_connection, execute_query, test_connection
+# Imports da base (postgres/)
+from .postgres.base import get_db_connection, execute_query, test_connection
 
-# Imports específicos por bloco
-from .ciclo_helper import get_dados_ciclo, insert_dados_ciclo, get_historico_ciclo
-from .momentum_helper import get_dados_momentum, insert_dados_momentum, get_historico_momentum
-from .risco_helper import get_dados_risco, insert_dados_risco, get_historico_risco
-from .tecnico_helper import get_dados_tecnico, insert_dados_tecnico, get_historico_tecnico
+# Imports específicos por bloco (postgres/)
+from .postgres.ciclo_helper import get_dados_ciclo, insert_dados_ciclo, get_historico_ciclo
+from .postgres.momentum_helper import get_dados_momentum, insert_dados_momentum, get_historico_momentum
+from .postgres.risco_helper import get_dados_risco, insert_dados_risco, get_historico_risco
+from .postgres.tecnico_helper import get_dados_tecnico, insert_dados_tecnico, get_historico_tecnico
 
 # Import cache consolidado (NOVO v1.0.14)
-from .scores_consolidados_helper import (
+from .postgres.scores_consolidados_helper import (
     get_score_cache_diario, save_score_cache_diario, 
     get_historico_scores, limpar_cache_antigo
 )
 
-# Imports utilitários
-from .utils import (
+# Imports utilitários (postgres/)
+from .postgres.utils import (
     check_database_health, get_all_latest_data, 
     create_tables_if_not_exist, insert_dados_exemplo
 )
