14e8be938709012c3e4ba3d2a0fcc97c4fd48e5a
Lindualdo <lindualdo@hotmail.com>
2025-06-08 21:01:21 +0100

    Fix: init_py

---
+++ app/services/utils/__init__.py
@@ -0,0 +1,18 @@
+# app/services/utils/__init__.py - CORRIGIDO v5.1.3
+
+from .helpers import *
+
+__all__ = [
+    "get_db_connection", "execute_query", "test_connection",
+    "get_dados_ciclo", "insert_dados_ciclo", "get_historico_ciclo",
+    "get_dados_momentum", "insert_dados_momentum", "get_historico_momentum", 
+    "get_dados_risco", "insert_dados_risco_completo", "get_historico_risco",
+    "get_dados_tecnico", "insert_dados_tecnico", "get_historico_tecnico",
+    "validate_nupl_value", "get_stats_nupl", "debug_ciclo_nupl",
+    "validate_sopr_value", "get_stats_sopr", "debug_momentum_sopr",
+    "insert_dados_momentum_legacy",
+    "get_score_cache_diario", "save_score_cache_diario", 
+    "get_historico_scores", "limpar_cache_antigo",
+    "check_database_health", "get_all_latest_data", 
+    "create_tables_if_not_exist", "insert_dados_exemplo"
+]
