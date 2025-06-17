75bb581ef2006abf01b65ca0fccff03d6ebb8fe0
Lindualdo <lindualdo@hotmail.com>
2025-06-17 22:26:23 +0100

    Update dash_main.py

--- app/routers/v3/dash_main.py
+++ app/routers/v3/dash_main.py
@@ -1,6 +1,6 @@
 # routers/v3/dash_home.py
 from fastapi import APIRouter
-from app.services.v3.dash_home_service import calcular_dashboard_v3, obter_dashboard_v3, debug_dashboard_v3
+from app.services.v3.dash_main import calcular_dashboard_v3, obter_dashboard_v3, debug_dashboard_v3 
 
 router = APIRouter()
 
