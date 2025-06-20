1b4bb9dcdae2cd3ba7df83692990fc1789b39213
Lindualdo <lindualdo@hotmail.com>
2025-06-20 22:03:06 +0100

    fix: sufixo do router

--- app/routers/dashboards.py
+++ app/routers/dashboards.py
@@ -6,21 +6,21 @@ from app.services.dashboards.dash_mercado_service   import processar_dash_mercad
 router = APIRouter()
 
 # processa as 4 camadas de analise (analise de mercado, de riscos, de alavancagem e execução tática)
-@router.post("/main")
+@router.post("/dash-main")
 async def post_dash_main():
     return processar_dash_main()
 
 # Obtem o dashboard principal (analise de mercado, de riscos, de alavancagem e execução tática)
-@router.get("/main")
+@router.get("/dash-main")
 async def get_dash_main():
     return obter_dash_main()
 
 # processa o dashboard de mercado (ciclo, momentum e tecnico - grava indicadores e scores)
-@router.post("/mercado")
+@router.post("/dash-mercado")
 async def post_dash_mercado():
     return processar_dash_mercado()
 
 # Obtem o dashboard de mercado (ciclo, momentum e tecnico - retorna indicadores e scores)
-@router.get("/mercado")
+@router.get("/dash-mercado")
 async def get_dash_mercado():
     return obter_dash_mercado()
