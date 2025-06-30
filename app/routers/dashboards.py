73b08aa710e972fba4d57e851f43198c7021814b
Lindualdo <lindualdo@hotmail.com>
2025-06-30 14:45:23 +0100

    Update dashboards.py

--- app/routers/dashboards.py
+++ app/routers/dashboards.py
@@ -23,9 +23,9 @@ async def post_dash_main():
 async def get_dash_main():
     return obter_dash_main()
 
-@router.post("/dash-mercado")
+@router.get("/dash-mercado")
 async def post_dash_mercado():
-    return processar_dash_mercado()
+    return obter_dash_mercado()
 
 @router.post("/dash-mercado")
 async def post_dash_mercado(aplicar_gatilho: bool = True):
