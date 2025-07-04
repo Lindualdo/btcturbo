022e44992af5de8f7441ec45a611b67eff1e041f
Lindualdo <lindualdo@hotmail.com>
2025-07-04 08:56:43 +0100

    Update tendencia.py

--- app/routers/tendencia.py
+++ app/routers/tendencia.py
@@ -4,7 +4,7 @@ from app.services.tendencia.tendecia_service import calcular_score
 
 router = APIRouter()
 
-@router.get("/calcular_score_tendecia")
+@router.get("/calcular-score-tendecia")
 async def calcular_score():
         return calcular_score()
   
